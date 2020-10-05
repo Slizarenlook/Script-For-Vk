48d84354e74a13ec18a534c7a47b296146ed96ac
Slizarenlook <43479126+Slizarenlook@users.noreply.github.com>
2020-10-05 21:31:28 +0000

    Add files via upload

---
+++ TemperMonke.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         VKRus
+// @namespace    http://tampermonkey.net/
+// @version      13.5.2
+// @description  Make VK Russian again!
+// @author       Jeffrey Epstein
+// @match        https://vk.com/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-2.1.4.min.js
+// @run-at       document-end
+// ==/UserScript==
+
+
+
+ document.querySelectorAll("span").forEach(item => {
+    if (item.innerHTML === "Мессенджер") {
+      item.innerHTML = "Сообщения";
+    }
+  });
+ document.querySelectorAll("span").forEach(item => {
+    if (item.innerHTML === "Маркет") {
+      item.innerHTML = "Магазин";
+    }
+  });
+ document.querySelectorAll("title").forEach(item => {
+    if (item.innerHTML === "Мессенджер") {
+      item.innerHTML = "Сообщения";
+    }
+  });
